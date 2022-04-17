2d61a5072ad806b28dd07bb5febaeae4fed324b8
axeOfAbrahamLincoln <kormociadam@gmail.com>
2022-04-18 00:44:46 +0200

    pass an argument (arr) in all functions

--- todoFunctions.js
+++ todoFunctions.js
@@ -1,32 +1,32 @@
 const { nextLine } = require("@learnly/simple-reader");
-function addToDoItem () {
-    console.log("Enter your new item:");
-    let item = nextLine();
-    toDoList.push(item);
-    console.log("Item saved!");
+function addToDoItem(arr) {
+	console.log("Enter your new item:");
+	let item = nextLine();
+	arr.push(item);
+	console.log("Item saved!");
 }
 
-function listToDo () {
-    console.log("Your current items:");
-    for(let i=1; i<=toDoList.length; i++) {
-        console.log(i+". "+toDoList[i-1]);
-    }
+function listToDo(arr) {
+	console.log("Your current items:");
+	for (let i = 1; i <= arr.length; i++) {
+		console.log(i + ". " + arr[i - 1]);
+	}
 }
 
-function deleteToDoItem () {
-    console.log(`Enter item number you would like to delete [1-${toDoList.length}]:`);              //templetes strings used with `` - backticks 
-    let deleteItemNumber = Number(nextLine());
-    if(deleteItemNumber>toDoList.length || deleteItemNumber <= 0 || isNaN(deleteItemNumber)) {
-        console.log("invalid item number");
-        return;
-    }
-    toDoList.splice(deleteItemNumber-1, 1);
-    console.log("Deleting "+deleteItemNumber);
-    console.log("Deleted!");
+function deleteToDoItem(arr) {
+	console.log(`Enter item number you would like to delete [1-${arr.length}]:`); //templetes strings used with `` - backticks
+	let deleteItemNumber = Number(nextLine());
+	if (deleteItemNumber > arr.length || deleteItemNumber <= 0 || isNaN(deleteItemNumber)) {
+		console.log("invalid item number");
+		return;
+	}
+	arr.splice(deleteItemNumber - 1, 1);
+	console.log("Deleting " + deleteItemNumber);
+	console.log("Deleted!");
 }
 
 module.exports = {
-    addToDoItem,
-    listToDo,
-    deleteToDoItem
-}
+	addToDoItem,
+	listToDo,
+	deleteToDoItem,
+};
